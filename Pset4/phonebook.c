ef5698c55e7aafebf1387d0d5e63bcb21329eda7
isaccsz <isaccruz47@outlook.com>
2023-07-18 17:43:24 -0300

    Added new example

---
+++ Pset4/phonebook.c
@@ -0,0 +1,24 @@
+#include <cs50.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+    
+    FILE *file = fopen("phonebook.csv", "a");
+    
+    if(file == NULL)
+    {
+        
+        return 1;
+        
+    }
+    
+    char *name = get_string("Nome: ");
+    char *number = get_string("Numero: ");
+    
+    fprintf(file, "%s, %s\n", name, number);
+    
+    fclose(file);
+    
+}
